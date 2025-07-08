1a73c3479eafd60d2aa0e26e904db0e3dae5e989
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-07-08 04:29:53 -0400

    6 done

--- bool06.py
+++ bool06.py
@@ -1,3 +1,4 @@
+a=4
 def main(a):
     """
     check the following statement "The variable "a" is an even number"
@@ -7,4 +8,5 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
+    return a%2!=1
+print(main(a))
